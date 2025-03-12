9ca9264aa45491b530b8dedee84ae27079f2d680
alex-kpify <alex@kpify.com.br>
2025-03-12 17:12:48 -0300

    gitignore atualizado

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+"""
+Script para extrair o áudio de um vídeo
+"""
 import os
-from moviepy.editor import *
-# video = VideoFileClip(os.path.join("TRF3-Gravação de Reunião.mp4"))
-# video.audio.write_audiofile(os.path.join("TRF3-Gravação de Reunião.mp3"))
-video = VideoFileClip(os.path.join("TRF4.mp4"))
-video.audio.write_audiofile(os.path.join("TRF4-Gravação de Reunião.mp3"))
+from moviepy.editor import VideoFileClip  # pylint: disable=E0401
+
+video = VideoFileClip(os.path.join("GT.mp4"))
+video.audio.write_audiofile(os.path.join("GT.mp3"))
